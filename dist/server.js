e3ec1d749e359cae0aa6ad771380fbc0043c973c
Pornmongkon Pongsai <pornmongkonp@gmail.com>
2019-10-03 16:53:24 +0700

    edit port

--- dist/server.js
+++ dist/server.js
@@ -1,18 +1,17 @@
 "use strict";
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
     return new (P || (P = Promise))(function (resolve, reject) {
         function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
         function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        function step(result) { result.done ? resolve(result.value) : new P(function (resolve) { resolve(result.value); }).then(fulfilled, rejected); }
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const Hapi = require("@hapi/hapi");
-const init = () => __awaiter(void 0, void 0, void 0, function* () {
+const init = () => __awaiter(this, void 0, void 0, function* () {
     const server = new Hapi.Server({
-        port: 3001,
+        port: process.env.PORT || 3001,
         host: '0.0.0.0',
     });
     server.route({
